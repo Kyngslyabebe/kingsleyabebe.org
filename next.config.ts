219cc01f61966b4020a74e2865248ddcd6ed420a
Kingsley Abebe <kingsleyabebe@hotmail.com>
2026-02-06 15:28:20 -0500

    Disable static page generation

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
+  experimental: {
+    staticPageGenerationTimeout: 0,
+  },
+  output: 'standalone',
 }
 
 module.exports = nextConfig
